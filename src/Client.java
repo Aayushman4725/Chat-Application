6d7c8fd5f98242d0715afa8099fe481256519d25
Aayushman Amatya <aayushmannp@gmail.com>
2023-12-27 20:59:28 +0545

    working application(one to one communication will be added later

--- src/Client.java
+++ src/Client.java
@@ -3,17 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.InetAddress;
+
 
 public class Client implements Runnable{
+
 
     private Socket client;
     private BufferedReader in;
     private PrintWriter out;
 
     private boolean done;
+
+
     @Override
     public void run(){
         try{
+            InetAddress IP=InetAddress.getLocalHost();
             client =new Socket("localhost",9999);
             out=new PrintWriter(client.getOutputStream(),true);
             in=new BufferedReader(new InputStreamReader(client.getInputStream()));
